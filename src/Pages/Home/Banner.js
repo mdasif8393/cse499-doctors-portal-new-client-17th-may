c0eb5bab2c804acf93e318128b93d99fcf83f8f8
unknown <md.asif.8393@gmail.com>
2023-06-07 23:07:49 +0600

    change name

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                     <img src="https://i.ibb.co/M7jvxSW/hospital.jpg" alt="" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                     <h1 className="text-5xl font-bold text-gray-800">Welcome to Worry-free Treatment</h1>
-                    <p className="py-6 text-gray-800">The Doctors Portal Institutes are regarded as one of the best heart hospitals in Bangladesh, performing a multitude of treatments and procedures in cardiology and cardiothoracic surgery. The scorecard shows an unmatched record of over 1,96,684 cardiac and cardiothoracic surgeries.</p>
+                    <p className="py-6 text-gray-800">The Sheba Portal Institutes are regarded as one of the best heart hospitals in Bangladesh, performing a multitude of treatments and procedures in cardiology and cardiothoracic surgery. The scorecard shows an unmatched record of over 1,96,684 cardiac and cardiothoracic surgeries.</p>
                     <Link to="/appointment"><PrimaryButton>Get STARTed</PrimaryButton></Link>
                     </div>
                 </div>
